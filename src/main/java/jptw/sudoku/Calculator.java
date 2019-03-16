81133477de312e911e2b1eeba9698a3ca381472f
Tomasz Witczak <TomaszMWitczak@gmail.com>
2019-03-16 17:49:50 +0100

    Remove redundant final specifier

--- src/main/java/jptw/sudoku/Calculator.java
+++ src/main/java/jptw/sudoku/Calculator.java
@@ -5,39 +5,39 @@ package jptw.sudoku;
 class Calculator {
     ////////////////////////////////////////////////////////////////// [Methods]
     //--------------------------------------------------------------- Addition <
-    final int add(final int a, final int b) {
+    int add(final int a, final int b) {
         return a + b;
     }
 
-    final double add(final double a, final double b) {
+    double add(final double a, final double b) {
         return a + b;
     }
 
     //------------------------------------------------------------ Subtraction <
-    final int diff(final int a, final int b) {
+    int diff(final int a, final int b) {
         return a - b;
     }
 
-    final double diff(final double a, final double b) {
+    double diff(final double a, final double b) {
         return a - b;
     }
 
     //--------------------------------------------------------- Multiplication <
-    final int mul(final int a, final int b) {
+    int mul(final int a, final int b) {
         return a * b;
     }
 
-    final double mul(final double a, final double b) {
+    double mul(final double a, final double b) {
         return a * b;
     }
 
     //--------------------------------------------------------------- Division <
-    final int div(final int a, final int b)
+    int div(final int a, final int b)
             throws ArithmeticException {
         return a / b;
     }
 
-    final double div(final double a, final double b) {
+    double div(final double a, final double b) {
         return a / b;
     }
 }
