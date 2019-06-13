0b7ca7af7ab07096db20a28fa6020f729bec8f9c
Tomasz Witczak <TomaszMWitczak@gmail.com>
2019-06-13 21:48:09 +0200

    Refactor BacktrackingSudokuSolver code

--- Model/src/main/java/jptw/sudoku/BacktrackingSudokuSolver.java
+++ Model/src/main/java/jptw/sudoku/BacktrackingSudokuSolver.java
@@ -1,39 +1,52 @@
-//////////////////////////////////////////////////////////////////////// Package
+///////////////////////////////////////////////////////////////////// Package //
 package jptw.sudoku;
 
-//////////////////////////////////////////////////////////////////////// Imports
+
+///////////////////////////////////////////////////////////////////// Imports //
 import javafx.util.Pair;
 
 import java.util.ArrayList;
 import java.util.Collections;
 
-/////////////////////////////////////////////////////////////// Class definition
+
+///////////////////////////////////////////// Class: BacktrackingSudokuSolver //
 public class BacktrackingSudokuSolver
         implements SudokuSolver {
-    ////////////////////////////////////////////////////////////////// [Methods]
-    //----------------------------------------------------- Main functionality <
+
+    //=========================================================== Behaviour ==//
+    //-------------------------------------------------------------- Main --==//
     @Override
-    public boolean solve(final SudokuBoard board) {
+    public
+    boolean solve
+            (final SudokuBoard board) {
 
         Pair<Integer, Integer> emptyFieldCoordinates
                 = findEmptyField(board);
 
         if (emptyFieldCoordinates == null) {
+
             if (board.equals(previousBoard)) {
                 return false;
             }
+
             previousBoard = new SudokuBoard(board);
+
             return true;
         }
 
         int i = emptyFieldCoordinates.getKey();
         int j = emptyFieldCoordinates.getValue();
 
-        ArrayList<Integer> randomSudokuNumbers = generateRandomSudokuNumbers();
+        ArrayList<Integer> randomSudokuNumbers
+                = generateRandomSudokuNumbers();
 
-        for (int randomSudokuNumber : randomSudokuNumbers) {
+        for (int randomSudokuNumber
+                : randomSudokuNumbers) {
+
             if (board.set(i, j, randomSudokuNumber)) {
+
                 if (solve(board)) {
+
                     return true;
                 }
             }
@@ -44,11 +57,20 @@ public class BacktrackingSudokuSolver
         return false;
     }
 
-    //------------------------------------------------------ Helper functions <<
-    private Pair<Integer, Integer> findEmptyField(final SudokuBoard board) {
 
-        for (int i = 0; i < BOARD_SIZE; i++) {
-            for (int j = 0; j < BOARD_SIZE; j++) {
+    //-------------------------------------------------- Helper functions --==//
+    private
+    Pair<Integer, Integer> findEmptyField
+            (final SudokuBoard board) {
+
+        for (int i = 0;
+             i < BOARD_SIZE;
+             i++) {
+
+            for (int j = 0;
+                 j < BOARD_SIZE;
+                 j++) {
+
                 if (board.get(i, j) == 0) {
                     return new Pair<>(i, j);
                 }
@@ -58,11 +80,17 @@ public class BacktrackingSudokuSolver
         return null;
     }
 
-    private ArrayList<Integer> generateRandomSudokuNumbers() {
+    private
+    ArrayList<Integer> generateRandomSudokuNumbers
+            () {
 
-        ArrayList<Integer> randomSudokuNumbers = new ArrayList<>();
+        ArrayList<Integer> randomSudokuNumbers
+                = new ArrayList<>();
 
-        for (int i = 0; i < BOARD_SIZE; i++) {
+        for (int i = 0;
+             i < BOARD_SIZE;
+             i++) {
+
             randomSudokuNumbers.add(i + 1);
         }
 
@@ -71,10 +99,17 @@ public class BacktrackingSudokuSolver
         return randomSudokuNumbers;
     }
 
-    /////////////////////////////////////////////////////////////////// [Fields]
-    private static final int BOARD_SIZE = 9;
-    private static SudokuBoard previousBoard = new SudokuBoard();
+
+    //================================================================ Data ==//
+    private static final
+    int BOARD_SIZE = 9;
+
+    private static
+    SudokuBoard previousBoard = new SudokuBoard();
+
 
 }
 
+
 ////////////////////////////////////////////////////////////////////////////////
+
