e5fbae39cbfb2f2f276e09b5286708b79ca61fd3
Tomasz Witczak <TomaszMWitczak@gmail.com>
2019-06-14 11:44:25 +0200

    Reformat Start code

--- View/src/main/java/sudoku/Start.java
+++ View/src/main/java/sudoku/Start.java
@@ -1,9 +1,8 @@
-////////////////////////////////////////////////////////////////////// | Package
+///////////////////////////////////////////////////////////////////// Package //
 package sudoku;
 
 
-////////////////////////////////////////////////////////////////////// | Imports
-
+///////////////////////////////////////////////////////////////////// Imports //
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -12,12 +11,10 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
-///////////////////////////////////////////////////////////////// | Class: Start
-public class Start
+//////////////////////////////////////////////////////////////// Class: Start //
+public
+class Start
         extends Application {
-
-
-    private static final Logger logger = Logger.getLogger(Start.class.getName());
 
 //    static {
 //        String configPath = Start.class.getClassLoader().getResource("./resources/sudoku/loggerConfig.properties").getFile();
@@ -25,26 +22,45 @@ public class Start
 //    }
 
 
-    //============================================================ | Behaviour <
-    public static void main(final String[] args) {
-        logger.log(Level.INFO, "Launch");
+    //=========================================================== Behaviour ==//
+    //-------------------------------------------------------------- Main --==//
+    public static
+    void main(final String[] args) {
+
+        logger.log(Level.INFO,
+                   "Launch");
+
         launch();
     }
 
     @Override
-    public void start(final Stage stage)
+    public
+    void start(final Stage stage)
             throws FileException {
+
         try {
             StageSetup.buildStage(stage,
-                    "Menu.fxml",
-                    "Sudoku");
-            logger.log(Level.INFO, "Setup stage");
+                                 "Menu.fxml",
+                                 "Sudoku");
+
+            logger.log(Level.INFO,
+                       "Setup stage");
+
         } catch (IOException e) {
-            logger.log(Level.SEVERE, "File exception", e);
+
+            logger.log(Level.SEVERE,
+                       "File exception",
+                       e);
+
             throw new FileException(e);
         }
-
     }
+
+    //================================================================ Data ==//
+    //------------------------------------------------------------ Logger --==//
+    private static final
+    Logger logger
+            = Logger.getLogger(Start.class.getName());
 
 
 }
