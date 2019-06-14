e0e1e32f85d6112ccc3abb83667786a76933da45
Tomasz Witczak <TomaszMWitczak@gmail.com>
2019-06-14 02:25:57 +0200

    Refactor FileSudokuBoardDao code

--- Model/src/main/java/jptw/sudoku/FileSudokuBoardDao.java
+++ Model/src/main/java/jptw/sudoku/FileSudokuBoardDao.java
@@ -1,52 +1,101 @@
+///////////////////////////////////////////////////////////////////// Package //
 package jptw.sudoku;
 
+
+///////////////////////////////////////////////////////////////////// Imports //
 import java.io.*;
 
-public class FileSudokuBoardDao
-        implements Dao<SudokuBoard>,
-        AutoCloseable {
 
+/////////////////////////////////////////////////// Class: FileSudokuBoardDao //
+public
+class FileSudokuBoardDao
+        implements AutoCloseable,
+                   Dao<SudokuBoard> {
 
-    public void finalize() throws Exception {
-        close();
-    }
+    //=========================================================== Behaviour ==//
+    //------------------------------------------------------ Constructors --==//
+    public
+    FileSudokuBoardDao
+            (final String filename) {
 
-    @Override
-    public void close() throws Exception {
-
-    }
-
-    public FileSudokuBoardDao(final String filename) {
         this.filename = filename;
     }
 
+
+    public
+    void finalize
+            ()
+            throws Exception {
+
+        close();
+    }
+
+
+    //--------------------------- Interface implementation: AutoCloseable --==//
     @Override
-    public SudokuBoard read() throws FileException {
+    public
+    void close
+            ()
+            throws Exception {
+    }
 
-        SudokuBoard sudokuBoard = null;
 
-        try (FileInputStream fileInputStream = new FileInputStream(filename);
-             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)) {
+    //------------------------ Interface implementation: Dao<SudokuBoard> --==//
+    @Override
+    public
+    SudokuBoard read
+            ()
+            throws FileException {
+
+        SudokuBoard sudokuBoard
+                = null;
+
+        try (FileInputStream fileInputStream
+                    = new FileInputStream(filename); 
+             ObjectInputStream objectInputStream
+                    = new ObjectInputStream(fileInputStream)) {
+
             sudokuBoard = (SudokuBoard) objectInputStream.readObject();
+
         } catch (IOException e) {
+
             throw new FileException(e);
+
         } catch (ClassNotFoundException e) {
+
             throw new RuntimeException(e);
         }
+
         return sudokuBoard;
     }
 
     @Override
-    public void write(final SudokuBoard obj) throws FileException {
+    public
+    void write
+            (final SudokuBoard obj)
+            throws FileException {
 
-        try (
-                FileOutputStream fileOutputStream = new FileOutputStream(filename);
-                ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream)) {
+        try (FileOutputStream fileOutputStream
+                    = new FileOutputStream(filename);
+             ObjectOutputStream objectOutputStream
+                    = new ObjectOutputStream(fileOutputStream)) {
+
             objectOutputStream.writeObject(obj);
+
         } catch (IOException e) {
+
             throw new FileException(e);
         }
     }
 
-    private String filename;
+
+    //================================================================ Data ==//
+    private
+    String filename;
+
+
 }
+
+
+////////////////////////////////////////////////////////////////////////////////
+
